9fa1649b1d38250c434064ba494f48c0dd0aa8c2
Aniket <aniketdjadhav2@gmail.com>
2022-12-04 23:15:56 +0530

    Added the delete request code :)

--- Arithmatic/src/main/java/com/jpa/test/controller/controller.java
+++ Arithmatic/src/main/java/com/jpa/test/controller/controller.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jpa.test.entities.input;
@@ -31,5 +33,16 @@ public class controller {
 		input=  this.service.getOperation(Id);
 		return input;
 		  
+	  }
+	  @DeleteMapping("deleteById/{Id}")
+	  @ResponseBody
+	  public  String deleteById(@PathVariable("Id") int Id) {
+		boolean ans=  this.service.deleteById(Id);
+		if(ans) {
+			return "deleted the Arithmatic whose Id is"+Id;
+		}
+		else {
+			return "Invalid Id Intered";
+		}
 	  }
 }
--- Arithmatic/src/main/java/com/jpa/test/service/service.java
+++ Arithmatic/src/main/java/com/jpa/test/service/service.java
@@ -2,6 +2,8 @@ package com.jpa.test.service;
 
 import java.util.Optional;
 
+import javax.xml.stream.XMLInputFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +14,7 @@ import com.jpa.test.userRepo.userRepo;
 public class service {
 	 @Autowired
 	 private userRepo userRepo;
-
+//---------------------------------------------Performing Operation And Storing data-----------------------//
 	public String performOperation(input in) {
 		// TODO Auto-generated method stub
 		int sol=0;
@@ -42,13 +44,33 @@ public class service {
 		}
 		
 	}
-
+	
+	
+	
+	
+//------------------------Get Operation By Id---------------------------------//
 	public Optional<input> getOperation(int id) {
 		System.out.println("inside service");
 		Optional<input> input=null;
-		// TODO Auto-generated method stub
 		input=this.userRepo.findById(id);
 		return input;
+	}
+	
+	
+	//--------------------Delete By Id--------------------------------------//
+
+	public boolean deleteById(int id) {
+		
+		Optional<input> input=null;
+		input=this.userRepo.findById(id);
+		if(input==null) {
+			return false;
+		}
+		else {
+			this.userRepo.deleteById(id);
+			return true;
+		}
+	
 	}
 	
 	 
